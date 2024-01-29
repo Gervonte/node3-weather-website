dfa11a9b217ac77bcdc7fb0ee0ffe4af54f79508
Gervonte Fowler <37189775+Gervonte@users.noreply.github.com>
2024-01-29 13:02:33 -0500

    Fix

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,14 +1,14 @@
-const request = require('request');
+const request = require('postman-request');
 
 const geocode = (address, callback) => {
 	const url =
 		'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-		address +
-		'.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1';
+		encodeURIComponent(address) +
+		'.json?access_token=pk.eyJ1IjoiZ2Vydm9udGUiLCJhIjoiY2xrYTNvOW50MDJtODNmcGwydmljcTV2ZSJ9.6X461imwit4l_ULM8Wz-jA&limit=1';
 
 	request({ url, json: true }, (error, { body }) => {
 		if (error) {
-			callback('Unable to connect to location services!', undefined);
+			callback('Unable to connect to location services', undefined);
 		} else if (body.features.length === 0) {
 			callback('Unable to find location. Try another search.', undefined);
 		} else {
